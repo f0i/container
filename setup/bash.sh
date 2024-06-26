a9d360c49230e00ba9f3cc01140543e932a28cbd
Martin Sigloch <f0i.git@projects.f0i.de>
2024-06-26 17:39:06 +0200

    Fix gitconfig setup

--- setup/bash.sh
+++ setup/bash.sh
@@ -6,5 +6,5 @@ echo 'export PATH=$PATH:$HOME/bin' >>~/.bashrc
 echo 'export LC_ALL=C.UTF-8' >>~/.bashrc
 echo '. /etc/bash_completion' >>~/.bashrc
 
-cp '/setup/.gitconfig /home/node/.gitconfig'
+cp '/setup/.gitconfig' '/home/node/.gitconfig'
 
